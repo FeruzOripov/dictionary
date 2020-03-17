e162cdff5f30594401e9a92a2793c65a6d7a1006
FeruzOripov <akaferuz99@gmail.com>
2020-03-17 20:49:55 +0500

    Adding TextField and ButtonBAr

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,51 @@ class MyAppHome extends StatefulWidget {
 }
 
 class _MyAppHomeState extends State<MyAppHome> {
+  final _searchFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello, World"),
+        title: Text("Simple Dictionary"),
       ),
-      body: Center(child: Text("Hello, World"),)
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child:
+              TextField(
+                controller: _searchFieldController,
+                decoration: InputDecoration(
+                  labelText: 'Enter a word',
+                ),
+              ),
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: Text('CLEAR'),
+                onPressed: () {
+                  _searchFieldController.clear();
+                },
+              ),
+              RaisedButton(
+                child: Text("SEARCH"),
+                onPressed: () {
+                  _fetchInfo(_searchFieldController.value.text);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
+  }
+
+  _fetchInfo(String word) {
+    print(word);
+
   }
 }
