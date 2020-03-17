e2235ea99a9d2e8df40297fb5bdc788f57178355
FeruzOripov <akaferuz99@gmail.com>
2020-03-17 12:37:27 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello, World",
+      home: MyAppHome(),
+    );
+  }
+}
+
+class MyAppHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, World"),
+      ),
+      body: Center(
+        child: Text("Hello, World"),
+      ),
+    );
+  }
+}
