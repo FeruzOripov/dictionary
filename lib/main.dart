506ffca272e35c38aafcdb942c7ad9a24000a747
FeruzOripov <akaferuz99@gmail.com>
2020-03-18 18:01:59 +0500

    Adding http request and navigation to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:http/http.dart" as http;
+import "dart:convert";
 
 void main() => runApp(MyApp());
 
@@ -19,6 +21,7 @@ class MyAppHome extends StatefulWidget {
 
 class _MyAppHomeState extends State<MyAppHome> {
   final _searchFieldController = TextEditingController();
+  List<dynamic> _definitions = [];
 
   @override
   Widget build(BuildContext context) {
@@ -61,8 +64,47 @@ class _MyAppHomeState extends State<MyAppHome> {
     );
   }
 
-  _fetchInfo(String word) {
+  _fetchInfo(String word) async {
     print(word);
+    final appId = '1aa4061a';
+    final appKey = '3b3d888192734986e2de4f5b711397df';
+    final response = await http.get('https://od-api.oxforddictionaries.com/api/v2/entries/en-us/' + word.toLowerCase(), headers: {'app_id': appId, 'app_key': appKey});
+    setState(() {
+      if (response.statusCode == 200) {
+        final results = json.decode(response.body);
+        _definitions = results['results'].first['lexicalEntries'].first["entries"].first['senses'].first['definitions'];
+      } else {
+        _definitions = [json.decode(response.body)['error']];
+      }
+      Navigator.push(
+          context,  MaterialPageRoute(
+            builder: (context) => DefinitionPage(word: word, definitions: _definitions),
+          ),
+        );
+    });
+  }
+}
 
+class DefinitionPage extends StatelessWidget {
+  List<dynamic> definitions = [];
+  String word = '';
+  DefinitionPage({Key key, this.definitions, this.word}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(word)),
+      body: Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: ListView.builder(
+                  itemBuilder: (context, position) {
+                    return Text(definitions[position]);
+                  },
+                  itemCount: definitions.length,
+                ),
+              ),
+            ),
+          );
   }
 }
